a3b2d6870c26cd28310b16f06a4e101efaf6ef52
Carlos Arruda <caugustogarruda@gmail.com>
2026-05-11 15:39:52 -0300

    Styling brands

--- brands/forms.py
+++ brands/forms.py
@@ -1,9 +1,17 @@
-from django.forms import ModelForm
+from django import forms
 from . import models
 
 
-class BrandForm(ModelForm):
+class BrandForm(forms.ModelForm):
 
     class Meta:
         model = models.Brand
         fields = ['name', 'description']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control'})
+        }
+        labels = {
+            'name': 'Nome',
+            'description': 'Descrição'
+        }
