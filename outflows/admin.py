d40024389fa3d69ae67d0ab525be0a274bb17dbb
Carlos Arruda <caugustogarruda@gmail.com>
2026-05-14 16:54:43 -0300

    Inflows CRUD and admins

--- outflows/admin.py
+++ outflows/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+class OutflowsAdmin(admin.ModelAdmin):
+    list_display = ('product', 'quantity', 'created_at', 'updated_at' )
+    search_fields = ('product__title',)
+
+admin.site.register(models.Outflow, OutflowsAdmin)
