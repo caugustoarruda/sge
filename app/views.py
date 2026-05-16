8df688b50e391e9f544580bc9a5f9f5c811b0ab5
Carlos Arruda <caugustogarruda@gmail.com>
2026-05-16 14:51:16 -0300

    Product metrics part 3

--- app/views.py
+++ app/views.py
@@ -1,19 +1,14 @@
 from django.shortcuts import render
-from products.models import Product
+from . import metrics
 
 
 def home(request):
-    product_metrics = {
-        'total_quantity': 1000,
-        'total_cost_price': 100000,
-        'total_selling_price': 300000,
-        'total_profit': 200000
-    }
     
     context = {
-        'product_metrics': product_metrics
+        'product_metrics': metrics.get_product_metrics()
     }
 
     return render(
         request, 'home.html', context
     )
+
