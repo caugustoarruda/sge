4cb340dc6b7d871771918687600df8c2b9fb1c95
Carlos Arruda <caugustogarruda@gmail.com>
2026-05-18 10:26:59 -0300

    Outflows inject sales metrics

--- outflows/views.py
+++ outflows/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import ListView, CreateView, DetailView
 from django.urls import reverse_lazy
 from . import models, forms
+from app.metrics import get_sales_metrics
 
 
 class OutflowListView(ListView):
@@ -17,6 +18,11 @@ class OutflowListView(ListView):
             queryset = queryset.filter(product__title__icontains=product)
         
         return queryset
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['sales_metrics'] = get_sales_metrics()
+        return context
     
 
 class OutflowCreateView(CreateView):
