63d298368485ef41b4c5bd4acaa8059ad75cddda
Carlos Arruda <caugustogarruda@gmail.com>
2026-05-19 10:11:58 -0300

    Adding chartjs part 2

--- app/views.py
+++ app/views.py
@@ -1,3 +1,4 @@
+import json
 from django.shortcuts import render
 from . import metrics
 
@@ -6,9 +7,10 @@ def home(request):
     
     context = {
         'product_metrics': metrics.get_product_metrics(),
-        'sales_metrics': metrics.get_sales_metrics()
+        'sales_metrics': metrics.get_sales_metrics(),
+        'daily_sales_data': json.dumps(metrics.get_daily_sales_data()),
     }
-
+    print(context['daily_sales_data'])
     return render(
         request, 'home.html', context
     )
