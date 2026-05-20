8f0bc5ab9434aaea29e3e1dbe9eef005d9acf7da
Carlos Arruda <caugustogarruda@gmail.com>
2026-05-20 09:09:28 -0300

    Login page

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth
 from . import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('login/', auth.LoginView.as_view(), name='login'),
 
     path('', views.home, name='home'),
 
